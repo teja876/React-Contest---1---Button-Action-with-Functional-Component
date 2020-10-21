be74220001e60636650c05193a4c2d92752e7e4a
Sai Teja <37472103+teja876@users.noreply.github.com>
2020-10-21 20:29:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,16 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [view, setView] = React.useState(false);
+  const handelClick = () => {
+    setView(!view);
+  }
   return (
-    <div id="main">
+    <div id="main"> 
       // Do not alter the main div
+      <button id="click" onClick={handelClick}>
+        (view === true) ? <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>: null
+      </button>
     </div>
   );
 }
